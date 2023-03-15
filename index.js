1f160f2b8fe7639c45e8535e0774163df52c00a5
JoshuaEgage <93669172+JoshuaEgage@users.noreply.github.com>
2023-03-15 16:33:46 -0500

    stuff

--- index.js
+++ index.js
@@ -2,9 +2,41 @@ import express from 'express';
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
+const dogs = [
+		{
+			"name": "dog 1",
+			"age": 10,
+			"color": "orange",
+			"adopted": true
+		},
+		{
+			"name": "king doggo",
+			"age": 5,
+			"color": "black",
+			"adopted": true
+		},
+		{
+			"name": "shameless dog",
+			"age": 1,
+			"color": "blue",
+			"adopted": false
+		}
+	]
+
 app.get('/', function(req, res) {
-	console.log('home');
-	res.send('homepage');
+	res.render('home', {dogs});
+});
+
+app.get('/about', function(req, res) {
+	res.render('about');
+});
+
+app.use(function(req, res) {
+	res.status(404).render('404');
 })
 
 app.listen(1990);
